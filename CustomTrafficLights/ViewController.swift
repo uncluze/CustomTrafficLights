df4a12fe693f2d1d262a5005a6b34eec139d53a8
uncluze <uncluze@yandex.ru>
2022-10-23 00:13:13 +0300

    Initial Commit

---
+++ CustomTrafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTrafficLights
+//
+//  Created by uncluze on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
