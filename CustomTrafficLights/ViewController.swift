95720bf0785782dd0d555ae910adf80c5784bdae
uncluze <uncluze@yandex.ru>
2022-10-23 19:44:08 +0300

    custom button

--- CustomTrafficLights/ViewController.swift
+++ CustomTrafficLights/ViewController.swift
@@ -29,15 +29,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        startButton.layer.cornerRadius = 10
+        startButton.configuration = setupButton(with: "Get Start")
+       // startButton.layer.cornerRadius = 10
         redLightView.alpha = lightOff
         yellowLightView.alpha = lightOff
         greenLightView.alpha = lightOff
+        
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
     }
 
     @IBAction func startButtonPressed(_ sender: UIButton) {
-    
-        startButton.setTitle("NEXT", for: .normal)
+        
+        startButton.configuration = setupButton(with: "NEXT")
+        // startButton.setTitle("NEXT", for: .normal)
         
         switch trafficLightColors {
         case .red:
@@ -56,3 +62,13 @@ class ViewController: UIViewController {
     }
 }
 
+private func setupButton(with title: String) -> UIButton.Configuration {
+    
+    var buttonConfiguration = UIButton.Configuration.filled()
+    buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.8093360066, green: 0.8442705274, blue: 0.9972055554, alpha: 1)
+    buttonConfiguration.title = title
+    buttonConfiguration.buttonSize = .large
+    buttonConfiguration.cornerStyle = .large
+    buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+    return buttonConfiguration
+}
